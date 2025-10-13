b454df549813cb2a7284a8c23e1fec0ce25a175b
kaf99 <this.noz99@gmail.com>
2025-10-13 23:32:50 +0530

    Update user.rb

--- app/models/spree/user.rb
+++ app/models/spree/user.rb
@@ -1,8 +1,8 @@
 class Spree::User < Spree.base_class
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable, :invitable,
 
   # Spree modules
   include Spree::UserAddress
