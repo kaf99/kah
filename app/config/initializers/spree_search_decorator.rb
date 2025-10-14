4e93c2a5ac784896512c6457faad8dba1b6fffed
kaf99 <this.noz99@gmail.com>
2025-10-15 04:53:52 +0530

    Add files via upload

---
+++ app/config/initializers/spree_search_decorator.rb
@@ -0,0 +1,8 @@
+# config/initializers/spree_search_decorator.rb
+
+Rails.application.config.to_prepare do
+  if defined?(Spree::Search::Base)
+    require_dependency Rails.root.join('app/models/spree/search/base_decorator')
+    Spree::Search::Base.prepend(Spree::Search::BaseDecorator)
+  end
+end
